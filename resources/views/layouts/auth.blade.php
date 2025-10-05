04612c48a940e585b61a58f54f338e4e816315a9
Alexanderjms <jasson.suazo@unah.hn>
2025-10-05 11:51:53 -0600

    fix(auth): update logo image source to use new SVG format

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -343,7 +343,7 @@
                 <div class="text-center mb-4">
                     <div
                         class="inline-flex items-center justify-center w-24 h-24 rounded-full mb-2 bg-gray-100 dark:bg-white border-2 border-white dark:border-gray-500 transition-colors">
-                        <img src="{{ $appLogoUrl ?? asset('images/logo.png') }}" alt="Logo" class="app-logo w-20 h-20 object-contain"
+                        <img src="{{ $appLogoUrl ?? asset('images/logo-hardlan-blue.svg') }}" alt="Logo" class="app-logo w-20 h-20 object-contain"
                             style="--app-logo-max: 80px;">
                     </div>
                     <h2 class="text-lg font-bold text-gray-800 dark:text-gray-100 serif-bold">
