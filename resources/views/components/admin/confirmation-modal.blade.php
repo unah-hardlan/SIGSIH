b913873d472d6c4b9f6ba2486ec87d9a319cbc60
Alexanderjms <jasson.suazo@unah.hn>
2025-10-12 19:54:48 -0600

    style: Enhance confirmation and form modal designs for improved responsiveness and user experience

--- resources/views/components/admin/confirmation-modal.blade.php
+++ resources/views/components/admin/confirmation-modal.blade.php
@@ -18,7 +18,7 @@
     @click="{{ $modalName }} = false"
     @keydown.window.escape="{{ $modalName }} = false"
     x-cloak>
-    <div class="bg-white dark:bg-gray-800 p-6 w-11/12 sm:w-full max-w-sm mx-auto" @click.stop> 
+    <div class="bg-white dark:bg-gray-800 p-6 w-11/12 sm:w-full max-w-sm mx-auto rounded-lg shadow-xl" @click.stop> 
         <div class="flex justify-between items-center border-b border-gray-200 dark:border-gray-700 pb-3">
             <h3 class="text-xl font-bold text-gray-700 dark:text-gray-200">{{ $title }}</h3>
             <button @click="{{ $modalName }} = false" class="text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 transition-colors duration-200"><i class="fas fa-times"></i></button>
@@ -27,8 +27,8 @@
             <p class="text-gray-600 dark:text-gray-300">{{ $message }} <strong class="text-gray-900 dark:text-gray-100" x-text="{{ $itemToDelete }} ? {{ $itemToDelete }}.{{ $itemNameProperty }} : ''"></strong>?</p>
         </div>
         <div class="flex justify-end pt-4">
-            <button type="button" @click="{{ $modalName }} = false" class="bg-gray-300 text-gray-800 px-4 py-2 rounded hover:bg-gray-400 dark:bg-gray-600 dark:text-gray-200 dark:hover:bg-gray-500 mr-2 transition-colors duration-200 ease-in-out">Cancelar</button>
-            <button type="button" @click="$dispatch('confirm-delete'); {{ $modalName }} = false" class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-600 transition-colors duration-200 ease-in-out">Confirmar</button>
+            <button type="button" @click="{{ $modalName }} = false" class="bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-400 dark:bg-gray-600 dark:text-gray-200 dark:hover:bg-gray-500 mr-2 transition-colors duration-200 ease-in-out">Cancelar</button>
+            <button type="button" @click="$dispatch('confirm-delete'); {{ $modalName }} = false" class="bg-red-600 text-white px-4 py-2 rounded-md hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-600 transition-colors duration-200 ease-in-out">Confirmar</button>
         </div>
     </div>
 </div>
