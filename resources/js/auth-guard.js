095852da21ea5f05fde0d456b23a8abd15e4b4e9
Alexanderjms <jasson.suazo@unah.hn>
2025-10-19 14:01:03 -0600

    refactor: Clean up code and improve readability in various files

--- resources/js/auth-guard.js
+++ resources/js/auth-guard.js
@@ -1,3 +1,1 @@
-// resources/js/auth-guard.js
-// auth-guard ya no valida token en localStorage; JWT sólo vive en cookie HttpOnly.
-(function () { /* Intencionalmente vacío; backend maneja expiración/redirecciones. */ })();
+(function () {})();
